1d664170cae1dddc3bd8c71dbfec6a1c0ed2dbda
ADT-ankita <55235936+ADT-ankita@users.noreply.github.com>
2021-10-02 19:20:48 +0530

    Hello World in c#

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+//Below is a Hello World code for C#
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}

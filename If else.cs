0a73eddf0b696944a317fe377a25e3db83a83eb8
ADT-ankita <55235936+ADT-ankita@users.noreply.github.com>
2021-10-02 19:26:54 +0530

    If Else in c#

---
+++ If else.cs
@@ -0,0 +1,17 @@
+//Below code demonstrates if else in c#
+
+using System;
+
+namespace IfElsedemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (2 > 1) 
+      {
+        Console.WriteLine("2 is greater than 1");
+      }    
+    }
+  }
+}
